121c14b032f8894dda3206592bd64d23846a8bf2
Janine White <janine.white@gmail.com>
2019-12-16 02:02:39 -0700

    Markers and header done

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,61 +1,139 @@
 const significant_earthquakes_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+const plates_json = "https://janinewhite.github.io/leaflet-challenge/assets/js/PB2002_plates.json";
+var start = [40.416775, -3.703790]
 
-var earthquakes, plates;
-// Arrays to hold earthquake and plate markers
-var quakeMarkers = [];
-var plateMarkers = [];
+// Create the tile layer that will be the background of our map
+var tileURL = "https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}";
+var tileAttribution = "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>";
+var lightmap = L.tileLayer(tileURL, {
+    attribution: tileAttribution,
+    maxZoom: 18,
+    id: "mapbox.light",
+    accessToken: API_KEY
+});
 
-function createMap() {
-    // Create the tile layer that will be the background of our map
-    var baseMapURL = "https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}"
-    var baseMapAttribution = "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>"
-    var baseMap = L.tileLayer(baseMapURL, {
-        attribution: baseMapAttribution,
-        maxZoom: 18,
-        id: "mapbox.light",
-        accessToken: API_KEY
+// Initialize LayerGroups
+var layers = {
+    GREEN: new L.LayerGroup(),
+    YELLOW: new L.LayerGroup(),
+    ORANGE: new L.LayerGroup(),
+    RED: new L.LayerGroup(),
+    PLATES: new L.LayerGroup()
+};
+
+// Create the map object with options
+var map = L.map("map-id", {
+    center: start,
+    zoom: 1,
+    layers: [
+        lightmap,
+        layers.GREEN,
+        layers.YELLOW,
+        layers.ORANGE,
+        layers.RED,
+        layers.PLATES
+    ]
+});
+
+// Add tile layer to map
+lightmap.addTo(map);
+
+// Create overlays obejct for layer control
+var overlays = {
+    "Green": layers.GREEN,
+    "Yellow": layers.YELLOW,
+    "Orange": layers.ORANGE,
+    "Red": layers.RED,
+    "Plates": layers.PLATES    
+};
+
+// Add layer control with overlays
+L.control.layers(null, overlays).addTo(map);
+
+// Add legend
+var info = L.control({position: "bottomleft"});
+info.onAdd = function() {
+    var div = L.DomUtil.create("div", "legend");
+    return div;
+};
+info.addTo(map);
+
+//Initialize icons for layer groups
+var icons = {
+    GREEN: L.ExtraMarkers.icon({
+        icon: "ion-md-pulse",
+        iconColor: "green",
+        markerColor: "black",
+        shape: "circle"
+    }),
+    YELLOW: L.ExtraMarkers.icon({
+        icon: "ion-md-pulse",
+        iconColor: "yellow",
+        markerColor: "black",
+        shape: "square"
+    }),
+    ORANGE: L.ExtraMarkers.icon({
+        icon: "ion-md-pulse",
+        iconColor: "orange",
+        markerColor: "black",
+        shape: "penta"
+    }),
+    RED: L.ExtraMarkers.icon({
+        icon: "ion-md-pulse",
+        iconColor: "red",
+        markerColor: "black",
+        shape: "star"
+    })
+};
+
+// Get earthquakes and tectonic plates
+d3.json(significant_earthquakes_url, function(earthquakesRes){
+    var generated = earthquakesRes.metadata.generated;
+    var earthquakes = earthquakesRes.features;
+    
+    d3.json(plates_json, function(platesRes){
+        //console.log(platesRes);
+        //var plates = platesRes.features;
+        
+        // Create object to count number of markers in layer
+        var markerCount = {
+            GREEN: 0,
+            YELLOW: 0,
+            ORANGE: 0,
+            RED: 0,
+        }
+        
+        // Assign status code for layer groups based on alert status
+        var statusCode;
+        for (var i = 0; i < earthquakes.length; i++) {
+            var quake = Object.assign({}, earthquakes[i]);
+            console.log(quake);
+            statusCode = quake.properties.alert.toUpperCase();
+            console.log("Alert: "+statusCode);
+            // Update marker count
+            markerCount[statusCode]++;
+            // Create marker for earthquake
+            var longitude = quake.geometry.coordinates[0];
+            var latitude = quake.geometry.coordinates[1];
+            var newMarker = L.marker([latitude, longitude], {
+                icon: icons[statusCode]
+            });
+            newMarker.addTo(layers[statusCode]);
+            var quakeTitle = quake.properties.title;
+            var quakeLink = quake.properties.url;
+            var quakeMagnitude = quake.properties.mag;
+            var quakePopup = `<a href="${quakeLink}">${quakeTitle}</a><br/>Magnitude: ${quakeMagnitude}`;
+            newMarker.bindPopup(quakePopup);
+        }
+        // Update legend
+        var legendText = [
+            "<p class='legend-header'>Alerts Updated</p>",
+            "<p class='legend-header'>"+moment.unix(generated).format("h:mm:ss A")+"</p>",
+            "<p><span class='green-markers'>Green:</span><span class='marker-count'> "+markerCount.GREEN+"</span><br/>",
+            "<span class='yellow-markers'>Yellow:</span><span class='marker-count'> "+markerCount.YELLOW+"</span><br/>",
+            "<span class='orange-markers'>Orange:</span><span class='marker-count'> "+markerCount.ORANGE+"</span><br/>",
+            "<span class='red-markers'>Red:</span><span class='marker-count'> "+markerCount.RED+"</span></p>"
+        ].join("");
+        document.querySelector(".legend").innerHTML = legendText;
     });
-    // Create a baseMaps object to hold the baseMap layer
-    var baseMaps = {"Base Map": baseMap};
-    // Create an overlayMaps objects to hold the earthquakes and plates layers
-    var overlayMaps = {"Earthquakes": earthquakes};
-    //var overlayMaps = {"Tectonic Plates": plates};
-    // Create the map object with options
-    var map = L.map("map-id", {
-        center: [33.231870, -111.790400],
-        zoom: 1,
-        layers: [baseMap, earthquakes]
-    });
-    // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
-    L.control.layers(baseMaps, overlayMaps, {collapsed: false}).addTo(map);
-}
-
-function createQuakeMarkers(response) {
-    // Pull the "features" property off of earthquakes response.data
-    var earthquakes = response.features;
-    console.log(earthquakes);    
-    // Loop through the stations array
-    for (var index = 0; index < earthquakes.length; index++) {
-        var earthquake = earthquakes[index];
-        // For each quake, create a marker and bind a popup with the quakes's location and magnitude
-        var latitude = earthquake.geometry.coordinates[0];
-        var longitude = earthquake.geometry.coordinates[1];
-        var locationName = earthquake.properties.title;
-        var magnitude = earthquake.properties.mag;
-        var popupTitle = "<h3>"+locationName
-            +"<br/>Latitude: "+latitude
-            +"<br/>Longitude: "+longitude
-            +"Magnitude: "+magnitude+"</h3>"
-        console.log(popupTitle);
-        var quakeMarker = L.marker([latitude,longitude]).bindPopup(popupTitle);
-        console.log(quakeMarker);
-        // Add the marker to the bikeMarkers array
-        quakeMarkers.push(quakeMarker);
-    }
-    // Create a layer group made from the bike markers array, pass it into the createMap function
-    createMap(L.layerGroup(quakeMarkers));
-}
-
-
-// Perform an API call to the Citi Bike API to get station information. Call createMarkers when complete
-d3.json(significant_earthquakes_url, createQuakeMarkers);
+});

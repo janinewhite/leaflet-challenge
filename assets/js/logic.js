adc3c317f6cb4be17696232eb831f0e46f2d2b32
Janine White <janine.white@gmail.com>
2019-12-16 21:28:13 -0700

    Plates working

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,7 +3,7 @@ const plates_json = "https://janinewhite.github.io/leaflet-challenge/assets/js/P
 var start = [40.416775, -3.703790]
 
 // Create the tile layer that will be the background of our map
-var tileURL = "https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}";
+var tileURL = "https://api.mapbox.com/styles/v1/jiwhite/ck49cfpnv0bf31cqsoquspbyy/tiles/256/{z}/{x}/{y}?access_token={accessToken}";
 var tileAttribution = "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>";
 var lightmap = L.tileLayer(tileURL, {
     attribution: tileAttribution,
@@ -47,16 +47,7 @@ var overlays = {
     "Plates": layers.PLATES    
 };
 
-// Add layer control with overlays
-L.control.layers(null, overlays).addTo(map);
 
-// Add legend
-var info = L.control({position: "bottomleft"});
-info.onAdd = function() {
-    var div = L.DomUtil.create("div", "legend");
-    return div;
-};
-info.addTo(map);
 
 //Initialize icons for layer groups
 var icons = {
@@ -90,26 +81,19 @@ var icons = {
 d3.json(significant_earthquakes_url, function(earthquakesRes){
     var generated = earthquakesRes.metadata.generated;
     var earthquakes = earthquakesRes.features;
-    
-    d3.json(plates_json, function(platesRes){
-        //console.log(platesRes);
-        //var plates = platesRes.features;
-        
-        // Create object to count number of markers in layer
-        var markerCount = {
-            GREEN: 0,
-            YELLOW: 0,
-            ORANGE: 0,
-            RED: 0,
-        }
+    // Create object to count number of markers in layer
+    var markerCount = {
+        GREEN: 0,
+        YELLOW: 0,
+        ORANGE: 0,
+        RED: 0,
+    }
         
         // Assign status code for layer groups based on alert status
         var statusCode;
         for (var i = 0; i < earthquakes.length; i++) {
             var quake = Object.assign({}, earthquakes[i]);
-            console.log(quake);
             statusCode = quake.properties.alert.toUpperCase();
-            console.log("Alert: "+statusCode);
             // Update marker count
             markerCount[statusCode]++;
             // Create marker for earthquake
@@ -135,5 +119,29 @@ d3.json(significant_earthquakes_url, function(earthquakesRes){
             "<span class='red-markers'>Red:</span><span class='marker-count'> "+markerCount.RED+"</span></p>"
         ].join("");
         document.querySelector(".legend").innerHTML = legendText;
-    });
 });
+
+//var plates = new L.LayerGroup();
+console.log(plates_json);
+d3.json(plates_json, function(platesRes){
+    console.log(platesRes);
+    L.geoJson(platesRes, {
+        color: "blue",
+        weight: 2
+    })
+    .addTo(layers["PLATES"]);
+    // Then add the tectonicplates layer to the map.
+    //plates.addTo(map);
+});
+
+// Add layer control with overlays
+//L.control.layers(null, overlays).addTo(map);
+L.control.layers(null,overlays).addTo(map);
+
+// Add legend
+var info = L.control({position: "bottomleft"});
+info.onAdd = function() {
+    var div = L.DomUtil.create("div", "legend");
+    return div;
+};
+info.addTo(map);
